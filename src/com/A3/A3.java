8327baa4736805be487c10e4060e79ea538da605
Liam Craft <crafty0410@gmail.com>
2020-11-02 22:16:04 +1100

    Objects and input
    Implemented Process objects with names. Refined input validation and implemented file reader method.

--- src/com/A3/A3.java
+++ src/com/A3/A3.java
@@ -4,65 +4,84 @@
 //Date: 1/11/2020
 package com.A3;
 
+import java.util.ArrayList;
+
 public class A3 {
 
     public static void main(String[] args) {
-        System.out.println("TESTING intellij");
+        //Process list
+        ArrayList<Process> processList = new ArrayList<Process>();
+
+        System.out.println("TESTING input....");
         //Check args exists, exit if not
-        if(args.length == 0){
+        if(args.length == 0) {
             System.out.println("No input arguments found. Please try again.");
         }
         else{
             for(int i = 0; i < args.length; i++) {
                 //check frame number input
                 if (i == 0) {
-                    if (isNumeric(args[i]) && Integer.parseInt(args[i]) > 0) {
+                    if (Process.isNumeric(args[i]) && Integer.parseInt(args[i]) > 0) {
                         //frame number ok, do whatever...
                         //TEST OUTPUT
                         System.out.println("Frame numbers are ok.");
+                        //
                     } else {
                         System.out.println("Error parsing first argument.");
                         System.out.println("First argument should be a positive integer representing the total frames.");
+
                     }
                 }
                 //check quantum size input
                 else if (i == 1) {
-                    if (isNumeric(args[1]) && Integer.parseInt(args[1]) > 0) {
+                    if (Process.isNumeric(args[1]) && Integer.parseInt(args[1]) > 0) {
                         //quantum size input is ok, do whatever.....
                         //TEST OUTPUT
                         System.out.println("Quantum size is ok.");
+                        //
                     } else {
                         System.out.println("Error parsing second argument.");
                         System.out.println("Second argument should be a positive integer representing the time quantum.");
+
                     }
                 }
                 //check the remaining input is correct format (name.txt)
                 else {
                     //check for correct file extension
-                    String check = args[i];
+                    String filePath = args[i];
                     //TEST OUTPUT
                     //System.out.println("Check substring: " + check.substring(check.lastIndexOf("."), check.length()));
-                    if (check.substring(check.lastIndexOf("."), check.length()).equals(".txt")) {
+                    if (filePath.substring(filePath.lastIndexOf("."), filePath.length()).equals(".txt")) {
                         //TEST OUTPUT
+                        System.out.println(filePath);
                         System.out.println("file extension ok.");
+                        //
+
+                        //Use file to build process objects - add them to the process list
+                        //make a process name
+                        String pName = filePath.substring(0,   filePath.lastIndexOf("."));
+                        //create a new process object
+                        Process p = new Process(Process.getInstructionList(filePath), pName);
+                        //add to the list
+                        processList.add(p);
+
+                        //TEST OUTPUT OF PROCESS OBJECTS
+                        int pCount = p.getProcessCount();
+                        for(int f = 0; f < pCount; f++){
+                            System.out.println(p.getName() + ": " + p.getInstruction(f));
+                        }
+                        //
                     } else {
                         System.out.println("Error parsing argument " + (i + 1) + ".");
                         System.out.println("Argument should be a .txt file.");
+
                     }
                 }
             }
         }
     }
     //utils
-    public static boolean isNumeric(String arg){
-        try{
-            Integer.parseInt(arg);
-            return true;
-        }
-        catch(NumberFormatException e){
-            return false;
-        }
-    }
+
 }
 
 
--- src/com/A3/Process.java
+++ src/com/A3/Process.java
@@ -5,7 +5,87 @@
 package com.A3;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Process {
+    ArrayList<Integer> pages;
+    String name;
+
+    //default
+    public Process(){
+        this.pages = new ArrayList<Integer>();
+        name = "default1";
+    }
+
+    //constructor
+    public Process(ArrayList<Integer> newPages){
+        this.pages = newPages;
+        name = "default2";
+    }
+    //constructor
+    public Process(ArrayList<Integer> newPages, String newName){
+        this.pages = newPages;
+        name = newName;
+    }
+
+    //getters
+    public ArrayList<Integer> getPages(){
+        return this.pages;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getProcessCount(){
+        return this.pages.size();
+    }
+    public int getInstruction(int i){
+        return this.pages.get(i);
+    }
+
+    //setters
+    public void setPages(ArrayList<Integer> newPages){
+        this.pages = newPages;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    //utility methods
+    public static ArrayList<Integer> getInstructionList(String filePath){
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        try{
+            FileReader fRead = new FileReader(filePath);
+            BufferedReader bRead = new BufferedReader(fRead);
+            //assign each line of the input file to an arraylist index
+            //Input files start with "begin", end with "end"
+            String input = bRead.readLine();
+            while(!input.equalsIgnoreCase("end")){
+                if(isNumeric(input)){
+                    result.add(Integer.parseInt(input));
+                }
+                input = bRead.readLine();
+            }
+        }
+        catch(IOException e){
+            System.out.println("IOException in Process.createProcessFromFile: " + e.getMessage());
+        }
+        catch(Exception e){
+            System.out.println("Exception in Process.createProcessFromFile: " + e.getMessage());
+        }
+        return result;
+    }
+
+    //
+    public static boolean isNumeric(String arg){
+        try{
+            Integer.parseInt(arg);
+            return true;
+        }
+        catch(NumberFormatException e){
+            return false;
+        }
+    }
+
 
 }
